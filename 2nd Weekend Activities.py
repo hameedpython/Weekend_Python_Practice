951fab3b7b00f61c8e56428fbe3f67d3e6fc5941
hameedpython <53147576+hameedpython@users.noreply.github.com>
2019-07-29 18:02:56 +0530

    Add files via upload

---
+++ 2nd Weekend Activities.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#numbers
+
+#integers
+2+7
+
+
+# In[2]:
+
+
+7-9
+
+
+# In[4]:
+
+
+#float 
+1.23+0.23
+
+
+# In[5]:
+
+
+0.1+0.2
+
+
+# In[6]:
+
+
+x=2
+y=3
+z=4
+#advance approach of assignment:
+x,y,z=2,3,4
+
+
+# In[7]:
+
+
+#constants in python
+
+eg: 
+    MAX_CONNECTIONS=100
+
+
+# In[8]:
+
+
+#INTro TO LIST DATATYPE
+
+bicycle=['hero','redline','trek','ranger']
+print(bicycle)
+
+
+# In[9]:
+
+
+print([0])
+
+
+# In[10]:
+
+
+print(bicycle[0])
+
+
+# In[11]:
+
+
+print(bicycle[3])
+
+
+# In[12]:
+
+
+print(bicycle[1].title())
+
+
+# In[13]:
+
+
+print(bicycle[1].upper())
+
+
+# In[ ]:
+
+
+
+
